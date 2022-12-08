36afa7a92530df257177a48e127970ecb73d5a50
towhid135 <towhidulislam252@gmail.com>
2022-12-08 21:36:14 +0600

    quick sort done

--- Sorting-Algorithms/Quick-sort.cpp
+++ Sorting-Algorithms/Quick-sort.cpp
@@ -1,32 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void partition(int ara[], int low, int high);
+int partition(int ara[], int low, int high);
 void print_ara(int ara[], int ara_size);
+void quick_sort(int ara[], int low, int high);
 int main(){
-    int ara[] = {2,9,4,4,4}, low = 0, high = 4;
-    partition(ara,low,high);
-    print_ara(ara,5);
+    int ara[] = {1,5,6,3,5,8,7,2,9}, low = 0, high = 8;
+    quick_sort(ara,low,high);
+    print_ara(ara,9);
     return 0;
 }
 
-void partition(int ara[], int low, int high){
+int partition(int ara[], int low, int high){
     int pivot = ara[high];
     int swap_index = low, i, temp;
 
-    for(i = 0; i < high; i++){
-        if(ara[i] < pivot){
-            temp = ara[swap_index];
-            ara[swap_index] = ara[i];
-            ara[i] = temp;
-
-            swap_index++;
+    //partition will happen from low to (high-1) of given array
+    for(i = low; i < high; i++){
+        if(ara[i] <= pivot){
+           /*if i==swap_index that means the value already on its position.
+           So, no need to swap just increase the swap_index because its a swap case.
+           */
+           if(i != swap_index){
+                temp = ara[swap_index];
+                ara[swap_index] = ara[i];
+                ara[i] = temp;
+           }
+           swap_index++;
         }
     }
 
     temp = ara[swap_index];
     ara[swap_index] = ara[high];
     ara[high] = temp;
+
+    return swap_index;
+}
+
+void quick_sort(int ara[], int low, int high){
+    if(low >= high) return;
+    int pivot_index;
+    pivot_index = partition(ara,low,high);
+    quick_sort(ara,low,pivot_index - 1);
+    quick_sort(ara,pivot_index + 1,high);
+    
 }
 
 void print_ara(int ara[], int ara_size){
---
+++ Sorting-Algorithms/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+cout<<"pivot_index: "<<pivot_index<<endl;
+    // //for left array
+    // quick_sort(ara,low,pivot_index - 1);
+    // quick_sort(ara,pivot_index+1,high);
