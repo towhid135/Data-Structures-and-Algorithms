c50808c81a53ba52cb4521dad1669dd6bb822406
towhid135 <towhidulislam252@gmail.com>
2023-01-21 09:12:50 +0600

    BFS initiated

---
+++ Graph/BFS/shortestPath.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void take_input(unordered_map< char,vector<char> > &graph);
+int main(){
+    unordered_map< char,vector<char> > graph;
+    freopen("input.txt","r",stdin);
+    take_input(graph);
+    
+}
+
+void take_input(unordered_map< char,vector<char> > &graph){
+    int numberOfLines, nodeNumber;
+    cin>>numberOfLines;
+}
